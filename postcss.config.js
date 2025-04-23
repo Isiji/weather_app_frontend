ac33bf9f907e5074421660a3748ddcc0804a0461
Isiji <blairisiji@gmail.com>
2025-04-23 12:36:10 +0300

    Initial commit - frontend

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {}, // ← Use the new plugin package here
+    autoprefixer: {},
+  },
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",  // <- this line is critical!
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
